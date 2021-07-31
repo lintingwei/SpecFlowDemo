4b421c66b2cfb23ec060e5644fc21f9f6bd5b49a
JerryLin <bggtank0102@gmail.com>
2021-07-31 22:12:34 +0800

    add first spec flow feature : Calculator

---
+++ SpecFlowDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpecFlowDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
