7c977fcf8a8cf5ad737498509e15d61abe9eed25
JerryLin <bggtank0102@gmail.com>
2021-07-31 23:33:54 +0800

    Add tow negtive numbers

--- SpecFlowDemoTest/Calculator.feature
+++ SpecFlowDemoTest/Calculator.feature
@@ -1,9 +1,16 @@
 ﻿Feature: Calculator
 	Simple calculator for adding two numbers
 
-@myCalculator
+@Calculator
 Scenario: Add two numbers
 	Given the first number is 50
 	And the second number is 70
 	When the two numbers are added
 	Then the result should be 120
+
+@Calculator
+Scenario: Add tow negtive numbers
+	Given the first number is -10
+	And the second number is -70
+	When the two numbers are added
+	Then the result should be -80
--- SpecFlowDemoTest/Calculator.feature.cs
+++ SpecFlowDemoTest/Calculator.feature.cs
@@ -75,11 +75,11 @@ namespace SpecFlowDemoTest
         
         [NUnit.Framework.TestAttribute()]
         [NUnit.Framework.DescriptionAttribute("Add two numbers")]
-        [NUnit.Framework.CategoryAttribute("myCalculator")]
+        [NUnit.Framework.CategoryAttribute("Calculator")]
         public virtual void AddTwoNumbers()
         {
             string[] tagsOfScenario = new string[] {
-                    "myCalculator"};
+                    "Calculator"};
             System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Add two numbers", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
 #line 5
@@ -113,6 +113,51 @@ this.ScenarioInitialize(scenarioInfo);
 #line hidden
 #line 9
  testRunner.Then("the result should be 120", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            }
+            this.ScenarioCleanup();
+        }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("Add tow negtive numbers")]
+        [NUnit.Framework.CategoryAttribute("Calculator")]
+        public virtual void AddTowNegtiveNumbers()
+        {
+            string[] tagsOfScenario = new string[] {
+                    "Calculator"};
+            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Add tow negtive numbers", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 12
+this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            bool isScenarioIgnored = default(bool);
+            bool isFeatureIgnored = default(bool);
+            if ((tagsOfScenario != null))
+            {
+                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((this._featureTags != null))
+            {
+                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((isScenarioIgnored || isFeatureIgnored))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 13
+ testRunner.Given("the first number is -10", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+#line 14
+ testRunner.And("the second number is -70", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 15
+ testRunner.When("the two numbers are added", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 16
+ testRunner.Then("the result should be -80", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line hidden
             }
             this.ScenarioCleanup();
