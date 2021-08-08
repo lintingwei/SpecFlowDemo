dfe0a769b6fa267dc39e5cc77b8760e8c7eb3316
JerryLin <bggtank0102@gmail.com>
2021-08-08 16:18:20 +0800

    Love Fifteen in green

--- SpecFlowDemo/TennisDisplayHelper.cs
+++ SpecFlowDemo/TennisDisplayHelper.cs
@@ -9,7 +9,7 @@ namespace SpecFlowDemo
         private static readonly Dictionary<int, string> ScoreMapping = new Dictionary<int, string>() { { 0, "Love" }, { 1, "Fifteen" }, { 2, "Thirty" }, { 3, "Forty" } };
         public static string GetDisplayScore(int playerAScore, int playerBScore)
         {
-            if (playerAScore > 0) {
+            if (playerAScore > 0 || playerBScore > 0) {
                 return $"{ScoreMapping[playerAScore]} {ScoreMapping[playerBScore]}";
             }
             return "Love All";
