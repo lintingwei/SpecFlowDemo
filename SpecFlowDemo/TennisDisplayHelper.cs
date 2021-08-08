77bc01c97a065babef71ae07b184e501db685dbf
JerryLin <bggtank0102@gmail.com>
2021-08-08 15:57:20 +0800

    Fifteen Love in green

--- SpecFlowDemo/TennisDisplayHelper.cs
+++ SpecFlowDemo/TennisDisplayHelper.cs
@@ -8,6 +8,9 @@ namespace SpecFlowDemo
     {
         public static string GetDisplayScore(int playerAScore, int playerBScore)
         {
+            if (playerAScore == 1) {
+                return "Fifteen Love";
+            }
             return "Love All";
         }
     }
