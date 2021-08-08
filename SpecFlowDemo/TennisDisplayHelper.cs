0e52014c0c9d0127eee05099d238c2881b70abb3
JerryLin <bggtank0102@gmail.com>
2021-08-08 16:23:41 +0800

    refactor

--- SpecFlowDemo/TennisDisplayHelper.cs
+++ SpecFlowDemo/TennisDisplayHelper.cs
@@ -9,13 +9,16 @@ namespace SpecFlowDemo
         private static readonly Dictionary<int, string> ScoreMapping = new Dictionary<int, string>() { { 0, "Love" }, { 1, "Fifteen" }, { 2, "Thirty" }, { 3, "Forty" } };
         public static string GetDisplayScore(int playerAScore, int playerBScore)
         {
-            if (playerAScore == playerBScore) {
+            if (SameScore(playerAScore, playerBScore))
+            {
                 return $"{ScoreMapping[playerAScore]} All";
             }
-            if (playerAScore > 0 || playerBScore > 0) {
-                return $"{ScoreMapping[playerAScore]} {ScoreMapping[playerBScore]}";
-            }
-            return "Love All";
+            return $"{ScoreMapping[playerAScore]} {ScoreMapping[playerBScore]}";
+        }
+
+        private static bool SameScore(int playerAScore, int playerBScore)
+        {
+            return playerAScore == playerBScore;
         }
     }
 }
