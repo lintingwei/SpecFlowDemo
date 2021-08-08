f35e20411c302ab2bd72bc81fbea25a552666e25
JerryLin <bggtank0102@gmail.com>
2021-08-08 16:29:17 +0800

    Deuce in green

--- SpecFlowDemo/TennisDisplayHelper.cs
+++ SpecFlowDemo/TennisDisplayHelper.cs
@@ -9,6 +9,9 @@ namespace SpecFlowDemo
         private static readonly Dictionary<int, string> ScoreMapping = new Dictionary<int, string>() { { 0, "Love" }, { 1, "Fifteen" }, { 2, "Thirty" }, { 3, "Forty" } };
         public static string GetDisplayScore(int playerAScore, int playerBScore)
         {
+            if (SameScore(playerAScore, playerBScore) && playerAScore > 2) {
+                return "Deuce";
+            }
             if (SameScore(playerAScore, playerBScore))
             {
                 return $"{ScoreMapping[playerAScore]} All";
