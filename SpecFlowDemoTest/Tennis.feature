a875e52e51e5a49482296ab0fffc704a12ce4468
JerryLin <bggtank0102@gmail.com>
2021-08-08 16:05:47 +0800

    Forty Love in red

--- SpecFlowDemoTest/Tennis.feature
+++ SpecFlowDemoTest/Tennis.feature
@@ -21,3 +21,10 @@ Scenario: PlayerA 2 And PlayerB 0
 	And playerB score 0
 	When get tennis score display string
 	Then the score display should be Thirty Love
+
+@Tennis
+Scenario: PlayerA 3 And PlayerB 0
+	Given playerA score 3
+	And playerB score 0
+	When get tennis score display string
+	Then the score display should be Forty Love
--- SpecFlowDemoTest/Tennis.feature.cs
+++ SpecFlowDemoTest/Tennis.feature.cs
@@ -207,6 +207,51 @@ this.ScenarioInitialize(scenarioInfo);
             }
             this.ScenarioCleanup();
         }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("PlayerA 3 And PlayerB 0")]
+        [NUnit.Framework.CategoryAttribute("Tennis")]
+        public virtual void PlayerA3AndPlayerB0()
+        {
+            string[] tagsOfScenario = new string[] {
+                    "Tennis"};
+            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("PlayerA 3 And PlayerB 0", null, tagsOfScenario, argumentsOfScenario, this._featureTags);
+#line 26
+this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            bool isScenarioIgnored = default(bool);
+            bool isFeatureIgnored = default(bool);
+            if ((tagsOfScenario != null))
+            {
+                isScenarioIgnored = tagsOfScenario.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((this._featureTags != null))
+            {
+                isFeatureIgnored = this._featureTags.Where(__entry => __entry != null).Where(__entry => String.Equals(__entry, "ignore", StringComparison.CurrentCultureIgnoreCase)).Any();
+            }
+            if ((isScenarioIgnored || isFeatureIgnored))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                this.ScenarioStart();
+#line 27
+ testRunner.Given("playerA score 3", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line hidden
+#line 28
+ testRunner.And("playerB score 0", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line hidden
+#line 29
+ testRunner.When("get tennis score display string", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line hidden
+#line 30
+ testRunner.Then("the score display should be Forty Love", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            }
+            this.ScenarioCleanup();
+        }
     }
 }
 #pragma warning restore
